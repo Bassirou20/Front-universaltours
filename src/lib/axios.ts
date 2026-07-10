9ef5dbbf04f9af383538d98f4f828e56c97c13ba
Bassirou20 <126479860+Bassirou20@users.noreply.github.com>
2026-07-10 01:45:25 +0000

    Update axios.ts remettre l'url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@
 import axios from "axios"
 
 export const API_BASE =
-  (import.meta as any).env.VITE_API_BASE_URL || "http://universaltours-api.duckdns.org/api"
+  (import.meta as any).env.VITE_API_BASE_URL || "http://127.0.0.1:8000/api"
 
 export const api = axios.create({
   baseURL: API_BASE,
